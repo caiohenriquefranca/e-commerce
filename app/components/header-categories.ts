e12c8118f33761de576a3b5790b0c30492332609
Caio Henrique <caiohenriquefrancav@icloud.com>
2025-01-14 18:05:49 -0300

    att

---
+++ app/components/header-categories.ts
@@ -0,0 +1,27 @@
+
+export const HeaderCategories: { title: string; href: string; description: string }[] = [
+    {
+      title: "shop",
+      href: "/",
+      description:
+        "A modal dialog that interrupts the user with important content and expects a response.",
+    },
+    {
+      title: "On Sale",
+      href: "/docs/primitives/hover-card",
+      description:
+        "For sighted users to preview content available behind a link.",
+    },
+    {
+      title: "New Arrivals",
+      href: "/docs/primitives/progress",
+      description:
+        "Displays an indicator showing the completion progress of a task, typically displayed as a progress bar.",
+    },
+    {
+      title: "Brands",
+      href: "/docs/primitives/scroll-area",
+      description: "Visually or semantically separates content.",
+    },
+    
+  ]
--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,13 +1,29 @@
-import { X } from "lucide-react"
+import { X } from "lucide-react";
+
 
 export function Header() {
-    return(
-        <header>
-            <div className=" flex justify-center items-center bg-black px-9 py-5">
-                <p className="text-white/90 text-xs">Sign up and get 20% off to your first order.</p>
-                <a href="" className="text-white text-xs ml-1 underline hover:text-white/80">Sign Up Now</a>
-                <X className="text-white justify-end"/>
-            </div>  
-        </header>
-    )
+  return (
+    <header>
+      <div className=" flex justify-center items-center bg-black py-3">
+        <p className="text-white/90 text-xs">
+          Sign up and get 20% off to your first order.
+        </p>
+        <a
+          href=""
+          className="text-white text-xs ml-1 underline hover:text-white/80"
+        >
+          Sign Up Now
+        </a>
+        <div className="flex justify-end ">
+          <X className="text-white text-xs" />
+        </div>
+      </div>
+      <div className="bg-white justify-between items-center py-10">
+        <div className="ml-52">
+          <a href="" className="text-4xl font-black">shop.co</a>
+        </div>
+    
+      </div>
+    </header>
+  );
 }
